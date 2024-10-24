d6abe844fd78fea4f597c1495684a01d3e6e087c
VectorRes <117469779+VectorRes@users.noreply.github.com>
2024-10-24 07:08:53 -0500

    Create instalacionDocker.sh

---
+++ instalacionDocker.sh
@@ -0,0 +1,7 @@
+sudo apt update
+sudo apt install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
