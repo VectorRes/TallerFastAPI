edfb75150a66f1fa179204b985472a84bae38b7d
VectorRes <117469779+VectorRes@users.noreply.github.com>
2024-10-24 10:18:36 -0500

    Update instalacionDocker.sh

--- instalacionDocker.sh
+++ instalacionDocker.sh
@@ -5,3 +5,7 @@ sudo systemctl enable docker
 
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.1.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+sudo chmod 660 /var/run/docker.sock
+sudo usermod -aG docker $USER
+newgrp docker
